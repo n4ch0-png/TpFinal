65d206e5c9aa59b1f828858a4d9425a9549b1113
n4ch0-png <ignaciopanelo114@gmail.com>
2025-12-01 15:24:01 -0300

    Update Task.ts

--- src/domain/Task.ts
+++ src/domain/Task.ts
@@ -1,4 +1,4 @@
-export type TaskStatus = "pendiente" | "en_progreso" | "completada";
+/*export type TaskStatus = "pendiente" | "en_progreso" | "completada";
 
 export interface Task {
     id: string;
@@ -9,4 +9,28 @@ export interface Task {
     fechaCreacion: string;
     fechaVencimiento?: string;
     eliminada?: boolean; 
+} */
+
+import { v4 as uuid } from "uuid";
+
+export class Task {
+  id: string;
+  titulo: string;
+  descripcion: string;
+  dificultad: 'baja' | 'media' | 'alta';
+  estado: TaskStatus;
+  fechaCreacion: Date;
+  vencimiento?: Date;
+  eliminada?: boolean; 
+
+  constructor(titulo: string, descripcion: string, dificultad: 'baja'|'media'|'alta', estado: TaskStatus, vencimiento?: Date, eliminada?: boolean) {
+    this.id = uuid();
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.dificultad = dificultad;
+    this.estado = estado;
+    this.fechaCreacion = new Date();
+    this.vencimiento = vencimiento;
+    this.eliminada = eliminada;
+  }
 }
