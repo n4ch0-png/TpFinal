97eb56b84730c9017ca8ef6fa0ab7806f81a83c4
Gab <proyectxs2000@gmail.com>
2025-12-01 06:35:58 +0000

    eliminar

--- dist/fuctional/taskFilters.js
+++
@@ -1,2 +0,0 @@
-export const filterBy = (prop, value) => (tasks) => tasks.filter(t => t[prop] === value);
-export const filterVencidas = (tasks) => tasks.filter(t => t.fechaVencimiento && new Date(t.fechaVencimiento) < new Date());
