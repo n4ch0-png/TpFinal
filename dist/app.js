c06b08f37583d8c02ebc0514ad6ab81b5e0782de
Gab <proyectxs2000@gmail.com>
2025-12-01 06:14:30 +0000

    limpio node_modules

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-import { TaskCollection } from "./domain/TaskCollection.js";
-import { mainMenu } from "./cli/menus.js";
-async function run() {
-    const collection = new TaskCollection();
-    await collection.init();
-    await mainMenu(collection);
-}
-run();
